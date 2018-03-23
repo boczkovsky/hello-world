d15983418fb404118952ee12563f217c5a2895b6
boczkovsky <mateusz.trojca@gmail.com>
2018-03-23 08:30:24 +0000

    some fixes

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 #testing the RPi configuration
 for i in range(10):
-    print('This is message number {} of Hello World! script'.format(1))
+    print("This is message number {} of Hello World! script".format(i))
